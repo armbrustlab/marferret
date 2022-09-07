f92e336397bcc774713400a6a97f26b1afa69bd8
blasks <blasks@uw.edu>
2022-09-07 16:14:10 -0700

    example script step with container

--- runall.sh
+++ runall.sh
@@ -1,2 +1,8 @@
 # commands to build database from raw sequence by calling each script in order
 # and pointing to raw sequence directory and supporting files in data/ directory
+
+MOUNT_DIR=$(realpath data)
+IMAGE=container/marferret.sif
+
+
+singularity exec --bind ${MOUNT_DIR} ${IMAGE} hmmer_domtblout2csv.py --options data/directory/input.tab
