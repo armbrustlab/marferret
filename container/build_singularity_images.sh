c4b2877c9905884555c305a3b625a9ec6e9e2e34
blasks <blasks@uw.edu>
2022-11-16 09:45:24 -0800

    updating singularity image commands

--- container/build_singularity_images.sh
+++ container/build_singularity_images.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# Use this command to build a singularity image from a 
-# Docker image tarball
+# Use these command to build a singularity images from
+# Docker image tarballs
 
 
 # build singularity image from tarball
-singularity build marferret.sif docker-archive://marferret.tar.gz
+singularity build mmseqs2.sif docker-archive://mmseqs2.tar.gz
+singularity build diamond.sif docker-archive://diamond.tar.gz
+singularity build hmmer.sif docker-archive://hmmer.tar.gz
+singularity build emboss.sif docker-archive://emboss.tar.gz
+singularity build marferret-py.sif docker-archive://marferret-py.tar.gz
