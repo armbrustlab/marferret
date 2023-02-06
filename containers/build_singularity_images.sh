8fef0c62b18f331a6dd606531a10134c4835f4eb
blasks <blasks@uw.edu>
2023-02-06 14:41:01 -0800

    renaming things

---
+++ containers/build_singularity_images.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Use these command to build a singularity images from
+# Docker image tarballs
+
+# build singularity image from tarball
+singularity build mmseqs2.sif docker-archive://mmseqs2.tar.gz
+singularity build diamond.sif docker-archive://diamond.tar.gz
+singularity build hmmer.sif docker-archive://hmmer.tar.gz
+singularity build emboss.sif docker-archive://emboss.tar.gz
+singularity build marferret-py.sif docker-archive://marferret-py.tar.gz
