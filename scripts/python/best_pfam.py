27f336f5f7e712b5a7607ffbcafc621a242575d6
Ryan D. Groussman <rgrous83@uw.edu>
2023-05-03 13:02:28 -0700

    Removed 'best frame' code
    Removed parameters and code for selecting the best nucleotide frame if the initial data was six-frame translated; the best-frame selection step was incorporated upstream in assemble_marferret.sh (with keep_longest_frame.py)

--- scripts/python/best_pfam.py
+++ scripts/python/best_pfam.py
@@ -15,11 +15,6 @@ def handle_arguments():
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument('input', type=str, help='Input domtblout file')
     parser.add_argument('output', type=str, help='Output file')
-    parser.add_argument(
-        '-f', '--frame_select',  
-        action='store_true', 
-        help='Frame-select output. For this to work, a single character frame designation must be the last character of each protein id'
-    )
     return parser
 
 def main():
@@ -45,21 +40,9 @@ def main():
     print('Selecting best annotation for each protein', flush=True)
     best_annot_df = df.iloc[df.groupby(['aa_id']).pfam_score.idxmax()]
     best_annot_df.reset_index(drop=True, inplace=True)
-    # if 6 frame translations were conducted
-    if args.frame_select:
-        print('Selecting best frame for each nucleotide translation', flush=True)
-        # separate nucleotide id from fram id
-        output_df = best_annot_df.copy()
-        output_df['nt_id'] = best_annot_df['aa_id'].str.slice(start=0, stop=-2)
-        output_df['frame'] = best_annot_df['aa_id'].str.slice(start=-1)
-        # take the best annotation from the 6 frames
-        output_df = output_df.iloc[output_df.groupby(['nt_id']).pfam_score.idxmax()]
-        output_df.reset_index(drop=True, inplace=True)
-    # else assign the best annotation to the output dataframe
-    else:
-        output_df = best_annot_df
+    
     # save results to a csv
-    output_df.to_csv(args.output, index=False)
+    best_annot_df.to_csv(args.output, index=False)
     print('Finished')
 
 if __name__ == "__main__":
