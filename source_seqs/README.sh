043db3285d45ef96bab61a2a36c801daba6791bf
Ryan D. Groussman <rgrous83@uw.edu>
2024-01-11 04:19:29 -0800

    Fixed wrapping README.sh

--- source_seqs/README.sh
+++ source_seqs/README.sh
@@ -1,7 +1,4 @@
 # Download the FASTA files for each entry to this /source_seqs/ directory
-# The /source_seqs/ contents should contain the files listed in the 'source_filename'
-# column of the metadata.csv file in the /data/ directory
+# The /source_seqs/ contents should contain the files listed in the 'source_filename' column of the metadata.csv file in the /data/ directory
 
-# FASTA files are only necessary for the entries with a 'Y' in the 'accepted' column;
-# they will be ingested when calling the 'assemble_marferret.sh' script. Entries with
-# a 'N' in the 'accepted' column are not necessary. 
+# FASTA files are only necessary for the entries with a 'Y' in the 'accepted' column; they will be ingested when calling the 'assemble_marferret.sh' script. Entries with a 'N' in the 'accepted' column are not necessary. 
